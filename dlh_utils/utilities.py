3a28307550787d639c5df8a0cbcfdd18e82d312a
hartj2 <Jenna.Hart@ons.gov.uk>
2023-06-07 15:54:10 +0100

    pylinting

--- dlh_utils/utilities.py
+++ dlh_utils/utilities.py
@@ -54,24 +54,24 @@ def list_files(file_path, walk=False, regex=None, full_path=True):
     std_out, std_error = process.communicate()
 
     try:
-      std_out = str(std_out).split("\\n")[:-1]
-      std_out[0] = std_out[0].strip("b'")
+        std_out = str(std_out).split("\\n")[:-1]
+        std_out[0] = std_out[0].strip("b'")
 
-      if full_path == True:
-          for i in std_out:
-              file_name = str(i).split(' ')[-1]
-              list_of_filenames.append(file_name)
+        if full_path == True:
+            for i in std_out:
+                file_name = str(i).split(' ')[-1]
+                list_of_filenames.append(file_name)
 
-      elif full_path == False:
-          for i in std_out:
-              file_name = str(i).split('/')[-1]
-              list_of_filenames.append(file_name)
+        elif full_path == False:
+            for i in std_out:
+                file_name = str(i).split('/')[-1]
+                list_of_filenames.append(file_name)
 
-      if regex != None:
-          list_of_filenames = list(filter(re.compile(regex).search, list_of_filenames))
-    except:
-      print('no files in this directory')
-      list_of_filenames = []
+        if regex != None:
+            list_of_filenames = list(filter(re.compile(regex).search, list_of_filenames))
+      except:
+        print('no files in this directory')
+        list_of_filenames = []
 
     return list_of_filenames
 
