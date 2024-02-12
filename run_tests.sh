baef184f728be324f101da62ae3d8c08cf32e6ee
cochrr <Rich.Cochrane@ons.gov.uk>
2024-02-12 14:43:34 +0000

    Test commit following umbrella issue

--- run_tests.sh
+++ run_tests.sh
@@ -4,3 +4,4 @@ pip3 install .
 cd ~/dlh_utils/dlh_utils/tests
 coverage run -m pytest .
 coverage report -m | grep -v local | grep -v cloudera | grep -v __ | grep -v conftest | grep -v tests | grep -v _version
+echo "TEST"
